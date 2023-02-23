0659c7708e23123cf0be0333b37609a0685d4a46
Reno_IM SUNG MIN <110653029+reno2104@users.noreply.github.com>
2023-02-23 14:02:06 +0900

    자식창-로그인버튼으로 부모창-로그인시키기

---
+++ 자식창이벤트로부모창로그인.js
@@ -0,0 +1,13 @@
+<body>
+<a href="loginConnect.html" onclick="submit()"><input type="submit" value="로 그 인" style="font-size:18pt; font-weight: 600;"></a>
+</body>
+
+
+
+<script>
+
+function submit(){
+	opener.location.href='/loginConnect.html'; 
+	window.close();
+}
+</script>
