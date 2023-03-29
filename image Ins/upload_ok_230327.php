cd409c315e30edc55f13c58c33d63ed87edd6fc5
Reno_IM SUNG MIN <110653029+reno2104@users.noreply.github.com>
2023-03-29 13:02:11 +0900

    image 서버에 저장하기

---
+++ image Ins/upload_ok_230327.php
@@ -0,0 +1,60 @@
+<?php
+// 설정
+$upload_path = '/var/www/html/img';
+// $uploads_dir = './uploads';
+$allowed_ext = array('jpg','jpeg','png','gif');
+ 
+// 변수 정리
+$error = $_FILES['myfile']['error'];
+$name = $_FILES['myfile']['name'];
+$ext = array_pop(explode('.', $name));
+ 
+// 오류 확인
+if( $error != UPLOAD_ERR_OK ) {
+	switch( $error ) {
+		case UPLOAD_ERR_INI_SIZE:
+		case UPLOAD_ERR_FORM_SIZE:
+			echo "파일이 너무 큽니다. ($error)";
+			break;
+		case UPLOAD_ERR_NO_FILE:
+			echo "파일이 첨부되지 않았습니다. ($error)";
+			break;
+		default:
+			echo "파일이 제대로 업로드되지 않았습니다. ($error)";
+	}
+	exit;
+}
+ 
+// 확장자 확인
+if( !in_array($ext, $allowed_ext) ) {
+	echo "허용되지 않는 확장자입니다.";
+	exit;
+}
+ 
+// 파일 이동
+// move_uploaded_file( $_FILES['myfile']['"$uploads_dir./.$name";tmp_name'], "$uploads_dir/$name");
+
+$uploads_dir = '/var/www/html/img';
+foreach ($_FILES["pictures"]["error"] as $key => $error) {
+    if ($error == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+        $name = $_FILES["pictures"]["name"][$key];
+        move_uploaded_file($_FILES['myfile']['tmp_name'], "$uploads_dir./.$name");
+    }
+}
+
+// "$uploads_dir./.$name";
+
+// 파일 정보 출력
+echo "<h2>파일 정보</h2>
+<ul>
+	<li>파일명: $name</li>
+	<li>확장자: $ext</li>
+	<li>파일형식: {$_FILES['myfile']['type']}</li>
+	<li>파일크기: {$_FILES['myfile']['size']} 바이트</li>
+ <li>파일저장경로: $uploads_dir/$name</li>
+ <li>에러수: $error</li> 
+ <li>임시저장경로: $tmp_name</li>
+ <li>$_FILES</li>
+</ul>";
+?>
